ecda5ffd86a4cebf8963a2fa09c2b5d46528842d
吴凡 <391855731@qq.com>
2022-04-24 11:16:23 +0800

    修改打包的jar包名称，启动脚本

--- lib/startup.sh
+++ lib/startup.sh
@@ -1,5 +1,5 @@
-pid=$(ps -ef | grep "connecotor-0.0.1-SNAPSHOT.jar" | grep -v grep | awk '{print $2}')
+pid=$(ps -ef | grep "mdm-connecotor-1.0.jar" | grep -v grep | awk '{print $2}')
 echo $pid
 kill -9 $pi
-nohup java -jar /esb/mdm-connector/connecotor-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev >/esb/mdm-connector/mdm-connector.log 2>&1 &
-tail -f /esb/mdm-connector/mdm-connector.log
+nohup java -jar ./mdm-connecotor-1.0.jar --spring.profiles.active=dev >./mdm-connector.log 2>&1 &
+tail -f ./mdm-connector.log
